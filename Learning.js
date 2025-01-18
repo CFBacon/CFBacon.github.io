feb6000377bdead5fab5b1148c35ab5127c4aa99
CFBacon <CFBacon@protonmail.com>
2025-01-18 12:37:25 -0500

    Add files via upload

---
+++ Learning.js
@@ -0,0 +1,6 @@
+function magic1(){
+    window.location.href = "https://chocolatefreakingbacon.com/";
+}
+function magic2(){
+    window.location.href = "https://www.facebook.com/profile.php?id=61569044466099"
+}
