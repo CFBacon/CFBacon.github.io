83551d9756c137f997b30fb93bcac6c8eaa7c2f9
CFBacon <CFBacon@protonmail.com>
2025-02-15 12:59:08 -0500

    Update Learning.js

--- Learning.js
+++ Learning.js
@@ -1,6 +1,1 @@
-function magic1(){
-    window.location.href = "https://chocolatefreakingbacon.com/";
-}
-function magic2(){
-    window.location.href = "https://www.facebook.com/profile.php?id=61569044466099"
-}
+
